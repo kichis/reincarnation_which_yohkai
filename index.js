1c3c1968ab291cb720fe062dafa14a9d1ffd2298
kichis <nk.wisteira2@gmail.com>
2021-01-19 11:11:48 +0800

    maxポイントが複数あった場合の処理完成

--- index.js
+++ index.js
@@ -7,25 +7,11 @@
 
 
 
-// const akaname = document.getElementById('akaname');
-// const kappa = document.getElementById('kappa');
-// const azuki = document.getElementById('azuki');
-// const amabie = document.getElementById('amabie');
-// const rokuro = document.getElementById('rokuro');
-// const zashiki = document.getElementById('zashiki');
-// const gasha = document.getElementById('gasha');
-// const nue = document.getElementById('nue');
-// const tukumo = document.getElementById('tukumo');
-// const kudan = document.getElementById('kudan');
-// const nekomata = document.getElementById('nekomata');
-// const bunpuku = document.getElementById('bunpuku');
-// const hitoku = document.getElementById('hitoku');
 
+// TODO:yokaiDatasのindex#=idになっているのでidを削除するか？？
 
+// !! 妖怪のid,名前,保有ポイントを保持するオブジェクト !!
 
-// TBD:yokaiDatasのindex#=idになっているのでidを削除するか？？
-
-// 妖怪のid,名前,保有ポイントを保持するオブジェクト
 let yokaiDatas = []
 const yokaiArry = [/*0*/"あかなめ", /*1*/"アマビエ", /*2*/"カッパ", /*3*/"小豆洗い", /*4*/"ろくろ首", /*5*/"座敷童", /*6*/"ガシャドクロ", /*7*/"件", /*8*/"猫又", /*9*/"分服茶釜", /*10*/"鵺", /*11*/"付喪神", /*12*/"一口おばけ"]
 for (let i = 0; i < yokaiArry.length; i++) {
@@ -37,52 +23,48 @@ for (let i = 0; i < yokaiArry.length; i++) {
     yokaiDatas.push(yokaiObj)
 }
 
-// 基本的にhtmlタグの中のonclick属性は使わない方がいい(使っても関数１つ)ようだが、タグの外の記述だとonclickが感知できなかったため、タグ内に記載することとする。
-// （JSで動的にレンダリングしているためか？）
-// 
-
 // !! function !!
 
+// onclickで発火する関数は、ポイント付与をするsetPointとコンポーネント遷移に繋がるlocation.hashで機能を分ける
+// (戻るボタンなど、コンポーネント遷移するがポイント付与はしない場合が想定されるため)
+
+// ※ポイント付与を伴わない、btn_startとq1はこのイベントリスナーに引っかからない(hashchange処理はhtmlタグ内で指示)
+$("[class^='btn_option']").on("click", function () {
+    // FIXME:この時点でsetPoint()実行後のポイントが付与されている。原因不明。
+    setPoint(this)
+    let nextid;
+    if ($(this).attr("class") == 'btn_option_#last') {
+                                                // 最後の問題の選択肢を押したとき!!
+
+
+    } else { 
+        nextid = $(this).attr("class").split("#")[1];
+    }
+    location.hash = "#" + nextid
+});
+
 // ポイント付与
-const addPoint = (/* ポイントを付与する妖怪のindex#が入ったArry = */ toWho, /*(int)*/point) => {
-    toWho.forEach(ele => {
-        yokaiDatas[ele].point += point
-    });
-    // console.log(yokaiDatas)
-}
-// btnからのvalを分解して変数に入れる(addPointで使う)
 const setPoint = (where) => {
     const arry = $(where).val().split("/");
     const featYokai = [arry[0]]
     const quantityPoint = Number(arry[1])
     const toWho = arry[2].split(".")
-    addPoint(toWho, quantityPoint)
     if (featYokai[0] !== '') {
-        addPoint(featYokai, 3)
+        featYokai.forEach(ele => { yokaiDatas[ele].point += 4 });
+        // addPoint(featYokai, 4)
     }
+    toWho.forEach(ele => { yokaiDatas[ele].point += quantityPoint });
+    // addPoint(toWho, quantityPoint)
+
+
+                                                // 編集中
+    console.log("{0} is in setPoint", yokaiDatas)
+    judge()
+
 }
-// ページ遷移 (現在のコンポーネントを非表示＆指定したコンポーネントを表示)
-const toNextPage = (nowid, nextid) => {
-    // setTimeout(function () {
-    $('#' + nowid).hide();
-    var next = document.getElementById(nextid);
-    $("#q_area").prepend(next);
-    next.style.display = "";
-    // }, 500);
-}
-
-
-// !! イベントリスナー !!
-
-// onclickで発火する関数は、ポイント付与をするsetPointとコンポーネント遷移をするhashchangeで機能を分ける
-// (戻るボタンなど、コンポーネント遷移するがポイント付与はしない場合が想定されるため)
-
-// ※btn_startとq1はこのイベントリスナーに引っかからない(hashchange処理はhtmlタグ内で指示)
-$("[class^='btn_option']").on("click", function () {
-    setPoint(this)
-    const nextid = $(this).attr("class").split("#")[1];
-    location.hash = "#" + nextid
-})
+// function addPoint(/* ポイントを付与する妖怪のindex#が入ったArry = */ toWho, /*(int)*/point) {
+//     toWho.forEach(ele => { yokaiDatas[ele].point += point });
+// }
 
 // URLの#変化を感知 -> 処理
 window.addEventListener('hashchange', e => {
@@ -93,13 +75,20 @@ window.addEventListener('hashchange', e => {
     // この時点では、hashは変更されているがコンポーネントは未変更である。よって、変数名(hash)とtoNextPageの引数名の対応関係が不自然になる。
     toNextPage(/*nowコンポーネントid*/oldHash, /*nextコンポーネントid*/newHash);
 }, false);
-    
+
+// ページ遷移 (現在のコンポーネントを非表示＆指定したコンポーネントを表示)
+const toNextPage = (nowid, nextid) => {
+    // setTimeout(function () {
+    $('#' + nowid).hide();
+    var next = document.getElementById(nextid);
+    $("#q_area").prepend(next);
+    next.style.display = "";
+    // }, 500);
+}    
 
 
 
-
-
-
+// !! ボタンクリック時のアニメーション !!
 
 // start
 $("#btn_start").on("click", function () {
@@ -110,83 +99,8 @@ $("#btn_start").on("click", function () {
     // toNextPage(btn_start, q1);
 })
 
-
-
-// q1_n
-
-
-// q2_海
-$("#q_2_s").on("click", function () {
-    addPoint_50("a");
-    addPoint_50("b");
-    addPoint_50("c");
-    toNextPage(q2, q3);
-})
-
-// q2_山
-$("#q_2_m").on("click", function () {
-    addPoint_50("d");
-    addPoint_50("e");
-    addPoint_50("f");
-    addPoint_50("g");
-    addPoint_50("h");
-    addPoint_50("i");
-    addPoint_50("j");
-    addPoint_50("k");
-    toNextPage(q2, q3);
-})
-
-// q3_人間
-$("#q_3_1").on("click", function () {
-    addPoint("a");
-    addPoint("b");
-    addPoint("d");
-    addPoint("e");
-    toNextPage(q3, q4);
-})
-// q3_貝
-$("#q_3_2").on("click", function () {
-    addPoint_8("c");
-    toNextPage(q3, q4);
-})
-// q3_その他（カッコイイ）
-$("#q_3_3").on("click", function () {
-    addPoint("f");
-    addPoint("g");
-    toNextPage(q3, q4);
-})
-// q3_その他（カワイイ）
-$("#q_3_4").on("click", function () {
-    addPoint("h");
-    addPoint("i");
-    addPoint("j");
-    addPoint("k");
-    toNextPage(q3, q4);
-})
-
-// q4_y
-$("#q_4_y").on("click", function () {
-    addPoint_3("a");
-    addPoint("b");
-    addPoint("f");
-    addPoint("g");
-    addPoint("i");
-    toNextPage(q4, q5);
-    setTimeout(function () {
-        const se1 = $('#hanauta');
-        se1[0].currentTime = 0;
-        se1[0].play();
-    }, 500);
-})
-// q4_n
-$("#q_4_n").on("click", function () {
-    addPoint("c");
-    addPoint("d");
-    addPoint("e");
-    addPoint("h");
-    addPoint("j");
-    addPoint("k");
-    toNextPage(q4, q5);
+// q5
+$(".btn_option_#q5").on("click", function () {
     setTimeout(function () {
         const se1 = $('#hanauta');
         se1[0].currentTime = 0;
@@ -194,214 +108,119 @@ $("#q_4_n").on("click", function () {
     }, 500);
 })
 
-// q5_y
-$("#q_5_y").on("click", function () {
-    addPoint("a");
-    addPoint_3("b");
-    addPoint("e");
-    addPoint("g");
-    addPoint("h");
-    addPoint("j");
-    addPoint("k");
 
 
-    toNextPage(q5, q6);
-})
-// q5_n
-$("#q_5_n").on("click", function () {
-    addPoint("c");
-    addPoint("d");
-    addPoint("f");
-    addPoint("i");
-    toNextPage(q5, q6);
-})
 
-// q6_y
-$("#q_6_y").on("click", function () {
-    addPoint("z");
-    toNextPage(q6, q7);
-})
-// q6_n
-$("#q_6_n").on("click", function () {
-    toNextPage(q6, q7);
-})
 
-// q7_y
-$("#q_7_y").on("click", function () {
-    addPoint("a");
-    addPoint("c");
-    addPoint_3("d");
-    addPoint("f");
-    addPoint("g");
-    addPoint("j");
-    addPoint("k");
-    toNextPage(q7, q8);
-})
-// q7_n
-$("#q_7_n").on("click", function () {
-    addPoint("b");
-    addPoint("e");
-    addPoint("h");
-    addPoint("i");
-    toNextPage(q7, q8);
-})
 
-// q8_y
-$("#q_8_y").on("click", function () {
-    addPoint("b");
-    addPoint_3("e");
-    addPoint("i");
-    addPoint("j");
-    addPoint("k");
-    toNextPage(q8, q9);
-})
-// q8_n
-$("#q_8_n").on("click", function () {
-    addPoint("a");
-    addPoint("c");
-    addPoint("d");
-    addPoint("f");
-    addPoint("g");
-    addPoint("h");
-    toNextPage(q8, q9);
-})
 
-// q9_ガイコツ
-$("#q_9_s").on("click", function () {
-    addPoint("b");
-    addPoint_3("f");
-    addPoint("j");
-    toNextPage(q9, q10);
-})
-// q9_中肉
-$("#q_9_m").on("click", function () {
-    addPoint("a");
-    addPoint("d");
-    addPoint("g");
-    addPoint("h");
-    addPoint("i");
-    toNextPage(q9, q10);
-})
-// q9_マシュマロ
-$("#q_9_l").on("click", function () {
-    addPoint("c");
-    addPoint("e");
-    addPoint("k");
-    toNextPage(q9, q10);
-})
 
-// q10_y
-$("#q_10_y").on("click", function () {
-    addPoint("c");
-    addPoint_3("e");
-    addPoint("g");
-    addPoint_3("i");
-    toNextPage(q10, q11);
-})
-// q10_n
-$("#q_10_n").on("click", function () {
-    addPoint("a");
-    addPoint("b");
-    addPoint("d");
-    addPoint("f");
-    addPoint("h");
-    addPoint("j");
-    addPoint("k");
-    toNextPage(q10, q11);
-})
+const calcFinalPoint = () => { 
+    
+    
+    
+}
+// ポイントを判定して結果の妖怪の(yokaiDatasの)index#を返す
 
-// q11_y
-$("#q_11_y").on("click", function () {
-    addPoint("b");
-    addPoint("d");
-    addPoint("f");
-    addPoint("g");
-    addPoint_3("j");
-    toNextPage(q11, q12);
-})
-// q11_n
-$("#q_11_n").on("click", function () {
-    addPoint("a");
-    addPoint("c");
-    addPoint("e");
-    addPoint("h");
-    addPoint("i");
-    addPoint("k");
-    toNextPage(q11, q12);
-})
+const judge = () => {
+    // const resultArry = yokaiDatas.map(x => x.point)
+    const resultArry = [0, 53, 52, 53, 1, 2, 3, 4, 5, 6, 7, 8]
+    console.log(resultArry)
+    
+    // 最大のポイント
+    const maxPoint = Math.max(...resultArry)
+    // 最大ポイントを保持しているindedx#ofresultArry
+    let maxIndexArry = []
+    let maxIndex = resultArry.indexOf(maxPoint, 0)
+    while (maxIndex >= 0) {
+        maxIndexArry.push(maxIndex)
+        maxIndex = resultArry.indexOf(maxPoint, maxIndex+1)
+    }
+    
+    // console.log(maxIndexArry)
+    
+    let result
+    let omikujiArry
+    
+    // MEMO: maxPointが70以上の場合、maxPointを保持している妖怪はアマビエである
+    if (maxPoint >= 71) {
+        // アマビエと同じ選択肢(「海」「貝」+１つ以上) -> アマビエ
+        return result = 1;
+        
+    } else if (maxPoint == 70) {
+        // アマビエと同じ選択肢(「海」「貝」のみ) -> カッパか小豆洗いかポイントが大きい方 
+        if (resultArry[2] == resultArry[3]) {
+            // カッパと小豆洗いのポイントが一緒だった場合
+            omikujiArry = [2, 3]
+            return result = whenMaxEqual(omikujiArry);
+        } else {
+            return result = resultArry[2] > resultArry[3] ? 2 : 3;
+        }
+        
+    } else if (70 > maxPoint && maxPoint >= 50) {
+        // (「海」 && !「貝」) -> ポイントが最大の海系妖怪
+        if (maxIndexArry.length > 1) {
+            // 最大ポイントが複数妖怪
+            return result = whenMaxEqual(maxIndexArry)
+        } else {
+            return result = maxIndexArry[0]
+        }
+    } else if (maxPoint < 50) { 
+        // 「山」50未満 -> 山系妖怪 -> アマビエを候補から外す(アマビエのポイントを0にする)
+        // ExAm = except Amabie
+        const resultArryExAm = resultArry.slice(1, 1, 0)
 
-// q12_y
-$("#q_12_y").on("click", function () {
-    addPoint("a");
-    addPoint("b");
-    addPoint("c");
-    addPoint("e");
-    addPoint("g");
-    addPoint("h");
-    addPoint("j");
-    addPoint_3("k");
-    toNextPage(q12, q13);
-})
-// q12_n
-$("#q_12_n").on("click", function () {
-    addPoint("d");
-    addPoint("f");
-    addPoint("i");
-    toNextPage(q12, q13);
-})
+        console.log(resultArryExAm)
 
-// q13_y
-$("#q_13_y").on("click", function () {
-    addPoint("c");
-    addPoint("e");
-    addPoint_3("g");
-    addPoint("i");
-    addPoint("j");
-    toNextPage(q13, q14);
-})
-// q13_n
-$("#q_13_n").on("click", function () {
-    addPoint("a");
-    addPoint("b");
-    addPoint("d");
-    addPoint("f");
-    addPoint("h");
-    addPoint("k");
-    toNextPage(q13, q14);
-})
+        // 最大のポイント
+        const maxPointExAm = Math.max(...resultArryExAm)
+        // 最大ポイントを保持しているindedx#ofresultArry
+        let maxIndexArryExAm = []
+        let maxIndexExAm = resultArryExAm.indexOf(maxPointExAm, 0)
+        while (maxIndexExAm >= 0) {
+            maxIndexArryExAm.push(maxIndexExAm)
+            maxIndexExAm = resultArryExAm.indexOf(maxPointExAm, maxIndexExAm + 1)
+        }
 
-// q14_広く
-$("#q_14_w").on("click", function () {
-    addPoint("a");
-    addPoint("d");
-    addPoint("f");
-    addPoint("g");
-    addPoint("j");
+        //  -> その他の妖怪で最大のポイントを保有している妖怪(max.15)
+        //  ※アマビエ以外の海系妖怪は選ばれる可能性がある
+        if (maxIndexArryExAm.length > 1) {
+            // 最大ポイントが複数妖怪
+            return result = whenMaxEqual(maxIndexArryExAm)
+        } else {
+            return result = maxIndexArryExAm[0]
+        }
+    }
 
-    checkZ();
-})
-// q14_狭く
-$("#q_14_d").on("click", function () {
-    addPoint("b");
-    addPoint("c");
-    addPoint("e");
-    addPoint_3("h");
-    addPoint("i");
-    addPoint("k");
 
-    checkZ();
-})
+    const calcMaxItsindexarry = (array) => {
+        // 最大のポイント
+        const max = Math.max(...array)
+        // 最大ポイントを保持している妖怪のindex#(@resultArry)
+        let maxIndex = array.indexOf(max, 0)
+        let maxIndexArray = []
+        while (maxIndex >= 0) {
+            maxIndexArray.push(maxIndex)
+            maxIndex = array.indexOf(max, maxIndex+1)
+        }
+        return [max, maxIndexArray]
+    }
 
-// 垢舐めでyes -> 垢舐め
-// 妖怪保有ポイントの最大値が50以上 -> 海系妖怪
-    // アマビエ保有ポイントが62以上 -> アマビエ
-    // アマビエ保有ポイントが61 -> カッパか小豆洗いかポイントが大きい方
-    // 50 <= アマビエポイント <= 60 -> ポイントが最大の妖怪 ※
-// 50未満 -> 山系妖怪 ※
-    // アマビエを候補から外す
-    // -> その他の妖怪で最大のポイントを保有している妖怪(max.15)
-    //    ※アマビエ以外の海系妖怪は選ばれる可能性がある
-// ※海系・山系問わず、max値複数の場合 -> randam関数で特定の値 -> 隠しキャラ”一口おばけ”
+
+    // ※海系・山系問わず、maxポイント複数の場合 -> maxポイントの妖怪の中からrandam関数で結果を出す 
+    // (この工程を経る場合のみ、隠しキャラ”一口おばけ”がでる可能性がある)
+    const whenMaxEqual = (/*maxポイントの妖怪のindex#が入った*/arry) => {
+        // 一口妖怪を選択肢にいれる
+        arry.push(12)
+        // 例:arry内の妖怪が3体 -> omikuji = 0 || 1 || 2
+        const omikuji = Math.floor(Math.random() * arry.length)
+        // 結果の妖怪のindex#を返す
+        return arry[omikuji]
+    }
+}
+
+
+// console.log(whenMaxEqual([0, 8, 9, 6, 10]))
 
 function checkZ() {
     if (localStorage.getItem("z") == 1) { //一口おばけになるか
@@ -417,40 +236,35 @@ function checkZ() {
     } else { //一口おばけにならない
         // pass
     }
-    console.log("hello");
+    // console.log("hello");
 
     // local storageのvalue = 妖怪のポイント
-    const a = localStorage.getItem("a");
-    const b = localStorage.getItem("b");
-    const c = localStorage.getItem("c");
-    const d = localStorage.getItem("d");
-    const e = localStorage.getItem("e");
-    const f = localStorage.getItem("f");
-    const g = localStorage.getItem("g");
-    const h = localStorage.getItem("h");
-    const i = localStorage.getItem("i");
-    const j = localStorage.getItem("j");
-    const k = localStorage.getItem("k");
+    // const a = localStorage.getItem("a");
+    // const b = localStorage.getItem("b");
+    // const c = localStorage.getItem("c");
+    // const d = localStorage.getItem("d");
+    // const e = localStorage.getItem("e");
+    // const f = localStorage.getItem("f");
+    // const g = localStorage.getItem("g");
+    // const h = localStorage.getItem("h");
+    // const i = localStorage.getItem("i");
+    // const j = localStorage.getItem("j");
+    // const k = localStorage.getItem("k");
 
-    const items = [a, b, c, d, e, f, g, h, i, j, k];
-    console.log(items);
-    console.log(localStorage.length);
+    // const items = [a, b, c, d, e, f, g, h, i, j, k];
+    // console.log(items);
+    // console.log(localStorage.length);
 
-    //ポイントの最大値をピックアップ
-    const aryMax = function (a, b) { return Math.max(a, b); }
-    let max = items.reduce(aryMax);
-    // const aryMin = function (a, b) { return Math.min(a, b); }
-    // let min = result.reduce(aryMin); 
 
     // localstorageのkeyを配列化
-    const keys = new Array;
-    key();
-    function key() {
-        for (let i = 0; i < localStorage.length; i++) {
-            const x = localStorage.key(i); //key名　取得
-            keys.push(x);
-        }
-    }
+    // const keys = new Array;
+    // key();
+    // function key() {
+    //     for (let i = 0; i < localStorage.length; i++) {
+    //         const x = localStorage.key(i); //key名　取得
+    //         keys.push(x);
+    //     }
+    // }
     // console.log(keys);
 
     // maxの値をもつkeyを抽出
@@ -461,8 +275,7 @@ function checkZ() {
 
     // maxの値をもつkeyが複数ある場合は、ランダム関数でindex番号を指定
     // 一口お化けを出現させる
-    const num = Math.floor(Math.random() * max_point.length)
-    console.log(max_point[num]);
+    // console.log(max_point[num]);
 
 
     // 妖怪別のページに移動
